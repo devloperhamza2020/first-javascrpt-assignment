d0344155f63cbbbd30399601f676ccde226ab2a5
mdamirhamza <mdahshawon12@gmail.com>
2021-01-23 14:18:55 +0600

    commited done

---
+++ assignment.js
@@ -0,0 +1,37 @@
+//  
+
+function kilometerToMeter(kilo){
+    var meter = kilo * 1000;
+    return meter;
+}
+/* var shawonCall = kilometerToMeter(12);
+console.log(shawonCall);
+var lovelyCall = kilometerToMeter(16);
+console.log(lovelyCall);
+*/
+
+
+var friends = ['amir', 'hamza', 'shawon', 'shahinur', 'akter', 'lovely', 'MrsHamza'];
+var maxword = friends[0];
+function megaFriend(friends){
+    for(var i = 0; i < friends.length; i++){
+        var element = friends(i);
+        if ( element.length > maxword.length){
+            maxword = element;
+        }
+    }
+    return element;
+}
+ /* var biggestName = megaFriend(friends);
+ console.log(biggestName);
+  */
+
+
+
+
+  
+
+//function budgetCalculator(watch, mobile, laptop);
+
+
+// function hotelCost ()
